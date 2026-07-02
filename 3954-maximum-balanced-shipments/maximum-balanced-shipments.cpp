e9a0366f5ce761add68a07d89574c673cb7cb602
loki0985 <gudipallylokeshreddy@gmail.com>
2026-07-02 20:16:37 +0530

    Time: 0 ms (100.00%) | Memory: 153.6 MB (95.43%) - LeetSync

---
+++ 3954-maximum-balanced-shipments/maximum-balanced-shipments.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxBalancedShipments(vector<int>& weight) {
+        int ans = 0;
+        int mx = 0;
+
+        for (int x : weight) {
+            mx = max(mx, x);
+
+            if (x < mx) {
+                ans++;
+                mx = 0; // start a new shipment
+            }
+        }
+
+        return ans;
+    }
+};
