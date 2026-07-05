81dc6343597bd573b046a751ac83d43dd3834364
loki0985 <gudipallylokeshreddy@gmail.com>
2026-07-05 21:16:12 +0530

    Time: 3 ms (38.04%) | Memory: 14.4 MB (15.95%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    vector<int> curr;
+
+    void dfs(vector<int>& candidates, int target, int start) {
+        if (target == 0) {
+            ans.push_back(curr);
+            return;
+        }
+
+        for (int i = start; i < candidates.size(); i++) {
+            if (candidates[i] > target) break;
+
+            // skip duplicates
+            if (i > start && candidates[i] == candidates[i - 1])
+                continue;
+
+            curr.push_back(candidates[i]);
+            dfs(candidates, target - candidates[i], i + 1);
+            curr.pop_back();
+        }
+    }
+
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        dfs(candidates, target, 0);
+        return ans;
+    }
+};
