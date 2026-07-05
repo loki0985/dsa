8c6b7090ede1d44e2f22993858f2b281c4d2024f
loki0985 <gudipallylokeshreddy@gmail.com>
2026-07-05 21:06:47 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (38.46%) - LeetSync

---
+++ 29-divide-two-integers/divide-two-integers.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        if (dividend == INT_MIN && divisor == -1)
+            return INT_MAX;
+
+        long long dvd = llabs((long long)dividend);
+        long long dvs = llabs((long long)divisor);
+
+        long long ans = 0;
+
+        for (int i = 31; i >= 0; i--) {
+            if ((dvd >> i) >= dvs) {
+                ans += (1LL << i);
+                dvd -= (dvs << i);
+            }
+        }
+
+        if ((dividend < 0) ^ (divisor < 0))
+            ans = -ans;
+
+        return (int)ans;
+    }
+};
